37a8361a22bdea42c3ba5b4a8cc80f688912f61f
Eunice Mata <eunice.mata2992@gmail.com>
2025-03-02 02:09:32 -0600

    cambio de color del laberinto

--- animacion.py
+++ animacion.py
@@ -1,11 +1,12 @@
 import matplotlib.pyplot as plt
 
 def animar_laberinto(laberinto, recorrido, solucion = None):
+    plt.xlabel("Laberinto " + str(len(laberinto)) + "x" + str(len(laberinto)))
     lab_copy = [row[:] for row in laberinto]
     for pos in recorrido:
         if lab_copy[pos[0]][pos[1]] == 0:
             lab_copy[pos[0]][pos[1]] = 4
-        plt.imshow(lab_copy, cmap='viridis')
+        plt.imshow(lab_copy, cmap='tab20b')
         plt.pause(0.01)
     
     if solucion:
@@ -14,7 +15,7 @@ def animar_laberinto(laberinto, recorrido, solucion = None):
             pos = nodo.estado
             lab_copy[pos[0]][pos[1]] = 5  # Camino solución
             nodo = nodo.padre
-            plt.imshow(lab_copy, cmap='viridis')
+            plt.imshow(lab_copy, cmap='tab20b')
             plt.pause(0.01)
     
     plt.xticks([])
