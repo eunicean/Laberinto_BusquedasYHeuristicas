0fd93f9377706d952e8671a7c37c71cf16641548
Eunice Mata <eunice.mata2992@gmail.com>
2025-03-02 18:37:04 -0600

    cambio en la forma de animacion para que sea más rápido

--- animacion.py
+++ animacion.py
@@ -1,23 +1,39 @@
 import matplotlib.pyplot as plt
 
-def animar_laberinto(laberinto, recorrido, solucion = None):
-    plt.xlabel("Laberinto " + str(len(laberinto)) + "x" + str(len(laberinto)))
+def animar_laberinto(laberinto, recorrido, solucion=None, bloque=50):
     lab_copy = [row[:] for row in laberinto]
-    for pos in recorrido:
+
+    plt.figure(figsize=(6, 6))
+
+    # Animación del recorrido explorado
+    for i, pos in enumerate(recorrido):
         if lab_copy[pos[0]][pos[1]] == 0:
-            lab_copy[pos[0]][pos[1]] = 4
-        plt.imshow(lab_copy, cmap='tab20b')
-        plt.pause(0.01)
-    
+            lab_copy[pos[0]][pos[1]] = 4  # Nodo explorado
+        if i % bloque == 0:
+            plt.clf()
+            plt.imshow(lab_copy, cmap='tab20b')
+            plt.title(f"Explorando... Nodo {i}/{len(recorrido)}")
+            plt.pause(0.001)
+
+    plt.clf()
+    plt.imshow(lab_copy, cmap='tab20b')
+    plt.title("Exploración completa")
+    plt.pause(1)
+
     if solucion:
         nodo = solucion
+        camino = []
         while nodo:
-            pos = nodo.estado
-            lab_copy[pos[0]][pos[1]] = 5  # Camino solución
+            camino.append(nodo.estado)
             nodo = nodo.padre
+        camino
+
+        for i, pos in enumerate(camino):
+            lab_copy[pos[0]][pos[1]] = 5  # Camino solución
+            plt.clf()
             plt.imshow(lab_copy, cmap='tab20b')
-            plt.pause(0.01)
-    
-    plt.xticks([])
-    plt.yticks([])
+            plt.title(f"Mostrando solución... Paso {i+1}/{len(camino)}")
+            plt.pause(0.02)
+
+    plt.title("¡Camino final encontrado!")
     plt.show()
