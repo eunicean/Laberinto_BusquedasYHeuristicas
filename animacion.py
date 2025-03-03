4a9d1b1f98a2469b48f8a83d936a5b141f03c7f5
Eunice Mata <eunice.mata2992@gmail.com>
2025-03-02 21:29:01 -0600

    sin valores en los ejes

--- animacion.py
+++ animacion.py
@@ -4,13 +4,16 @@ def animar_laberinto(laberinto, recorrido, solucion=None, bloque=50):
     lab_copy = [row[:] for row in laberinto]
 
     plt.figure(figsize=(6, 6))
-
+    plt.xticks([])
+    plt.yticks([])
     # Animación del recorrido explorado
     for i, pos in enumerate(recorrido):
         if lab_copy[pos[0]][pos[1]] == 0:
             lab_copy[pos[0]][pos[1]] = 4  # Nodo explorado
         if i % bloque == 0:
             plt.clf()
+            plt.xticks([])
+            plt.yticks([])
             plt.imshow(lab_copy, cmap='tab20b')
             plt.title(f"Explorando... Nodo {i}/{len(recorrido)}")
             plt.pause(0.001)
@@ -31,6 +34,8 @@ def animar_laberinto(laberinto, recorrido, solucion=None, bloque=50):
         for i, pos in enumerate(camino):
             lab_copy[pos[0]][pos[1]] = 5  # Camino solución
             plt.clf()
+            plt.xticks([])
+            plt.yticks([])
             plt.imshow(lab_copy, cmap='tab20b')
             plt.title(f"Mostrando solución... Paso {i+1}/{len(camino)}")
             plt.pause(0.02)
